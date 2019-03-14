1331a10876fd440a298c6ad100bfcaf071901f9e
Francisco Costa <francisco@fcosta.pt>
2019-03-14 10:19:59 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,3 +5,15 @@ end
 def array_with_two_elements
   array_with_two = [1,2]
 end
+
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
